b8dd53d619e4c6b66f40b90fb36fc84e0264fcff
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:12:08 +0300

    1

---
+++ IntExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class IntExtensions
+{
+    public static event EventHandler<int> NumberChecked;
+
+    public static bool IsPrime(this int number)
+    {
+        bool isPrime = true;
+        if (number <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        NumberChecked?.Invoke(null, number);  
+        return isPrime;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        IntExtensions.NumberChecked += NumberCheckedHandler;
+
+        int numberToCheck = 17;
+
+        if (numberToCheck.IsPrime())
+        {
+            Console.WriteLine($"{numberToCheck} є простим числом.");
+        }
+        else
+        {
+            Console.WriteLine($"{numberToCheck} не є простим числом.");
+        }
+    }
+
+    static void NumberCheckedHandler(object sender, int number)
+    {
+        Console.WriteLine($"Подія: перевірка числа {number} на простоту.");
+    }
+}
