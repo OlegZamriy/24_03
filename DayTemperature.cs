9f7a5fb74e9a9aab4f044b16cc499ffc12e7487a
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:06:53 +0300

    1

---
+++ DayTemperature.cs
@@ -0,0 +1,13 @@
+﻿public class DayTemperature
+{
+    public int Day { get; set; }
+    public int MaxTemperature { get; set; }
+    public int MinTemperature { get; set; }
+
+    public DayTemperature(int day, int maxTemperature, int minTemperature)
+    {
+        Day = day;
+        MaxTemperature = maxTemperature;
+        MinTemperature = minTemperature;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        DayTemperature[] temperatures = new DayTemperature[]
+        {
+            new DayTemperature(1, 30, 15),
+            new DayTemperature(2, 25, 10),
+            new DayTemperature(3, 35, 20),
+            new DayTemperature(4, 20, 5),
+            new DayTemperature(5, 28, 12)
+        };
+
+        var daysWithMaxDifference = temperatures
+            .Select(temp => new { Day = temp.Day, Difference = temp.MaxTemperature - temp.MinTemperature })
+            .OrderByDescending(d => d.Difference)
+            .First();
+
+        Console.WriteLine($"День з максимальною різницею температур: {daysWithMaxDifference.Day}");
+        Console.WriteLine($"Різниця між найвищою та найнижчою температурою: {daysWithMaxDifference.Difference} градусів");
+    }
+}
