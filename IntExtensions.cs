76834245c92c4265e1f2397ee9d7a5f3db969a10
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:11:06 +0300

    1

---
+++ IntExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class IntExtensions
+{
+    public static event EventHandler<int> NumberChecked;
+
+    public static bool IsEven(this int number)
+    {
+        bool isEven = number % 2 == 0;
+        NumberChecked?.Invoke(null, number);  
+        return isEven;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        IntExtensions.NumberChecked += NumberCheckedHandler;
+
+        int numberToCheck = 12;
+
+        if (numberToCheck.IsEven())
+        {
+            Console.WriteLine($"{numberToCheck} є парним числом.");
+        }
+        else
+        {
+            Console.WriteLine($"{numberToCheck} є непарним числом.");
+        }
+    }
+
+    static void NumberCheckedHandler(object sender, int number)
+    {
+        Console.WriteLine($"Подія: перевірка числа {number} на парність.");
+    }
+}
