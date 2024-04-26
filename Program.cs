749d23c6083a398623dee420e6f536b3e4ca83fd
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:13:11 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        StringExtensions.VowelCounted += VowelCountedHandler;
+
+        string text = "Це є приклад рядка для підрахунку голосних літер.";
+
+        int vowelCount = text.CountVowels();
+        Console.WriteLine($"Кількість голосних літер у рядку: {vowelCount}");
+    }
+
+    static void VowelCountedHandler(object sender, int count)
+    {
+        Console.WriteLine($"Подія: підрахунок голосних літер у рядку - {count}");
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static event EventHandler<int> VowelCounted;
+
+    public static int CountVowels(this string input)
+    {
+        int vowelCount = 0;
+        foreach (char c in input.ToLower())
+        {
+            if ("aeiou".Contains(c))
+            {
+                vowelCount++;
+            }
+        }
+
+        VowelCounted?.Invoke(null, vowelCount);  
+        return vowelCount;
+    }
+}
