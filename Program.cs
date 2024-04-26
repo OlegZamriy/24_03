773262eae3d4d596ba063a7626ae50de76698e28
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:04:01 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Це приклад рядка для тестування методу LengthOfLastWord.";
+        int lengthOfLastWord = text.LengthOfLastWord();
+        Console.WriteLine($"Довжина останнього слова в рядку: {lengthOfLastWord}");
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static int LengthOfLastWord(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return 0;
+        }
+
+        string[] words = input.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (words.Length == 0)
+        {
+            return 0;
+        }
+
+        string lastWord = words[words.Length - 1];
+        return lastWord.Length;
+    }
+}
