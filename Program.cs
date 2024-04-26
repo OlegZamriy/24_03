8c9fbd97a677c631725398002c1783562ef1e9f8
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:14:22 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        StringExtensions.ConsonantCounted += ConsonantCountedHandler;
+
+        string text = "Це є приклад рядка для підрахунку приголосних літер.";
+
+        int consonantCount = text.CountConsonants();
+        Console.WriteLine($"Кількість приголосних літер у рядку: {consonantCount}");
+    }
+
+    static void ConsonantCountedHandler(object sender, int count)
+    {
+        Console.WriteLine($"Подія: підрахунок приголосних літер у рядку - {count}");
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static event EventHandler<int> ConsonantCounted;
+
+    public static int CountConsonants(this string input)
+    {
+        int consonantCount = 0;
+        foreach (char c in input.ToLower())
+        {
+            if ("bcdfghjklmnpqrstvwxyz".Contains(c))
+            {
+                consonantCount++;
+            }
+        }
+
+        ConsonantCounted?.Invoke(null, consonantCount);  
+        return consonantCount;
+    }
+}
