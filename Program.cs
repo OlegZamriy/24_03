735285e461b62fe9ff6e7159f529511bc62836d3
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:15:21 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        StringExtensions.SentenceCounted += SentenceCountedHandler;
+
+        string text = "Це є приклад рядка з двома реченнями. Це друге речення!";
+
+        int sentenceCount = text.CountSentences();
+        Console.WriteLine($"Кількість речень у рядку: {sentenceCount}");
+    }
+
+    static void SentenceCountedHandler(object sender, int count)
+    {
+        Console.WriteLine($"Подія: підрахунок речень у рядку - {count}");
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static event EventHandler<int> SentenceCounted;
+
+    public static int CountSentences(this string input)
+    {
+        int sentenceCount = 0;
+        foreach (char c in input)
+        {
+            if (c == '.' || c == '?' || c == '!')
+            {
+                sentenceCount++;
+            }
+        }
+
+        SentenceCounted?.Invoke(null, sentenceCount);  
+        return sentenceCount;
+    }
+}
