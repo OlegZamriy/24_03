7883bf460086c1712b565e3f2cf52e11eb1758e4
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:08:07 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        StudentGrade[] students = new StudentGrade[]
+        {
+            new StudentGrade("Іван", new int[] { 85, 90, 92 }),
+            new StudentGrade("Марія", new int[] { 78, 85, 80 }),
+            new StudentGrade("Петро", new int[] { 92, 95, 88 })
+        };
+
+        var studentWithMaxGrade = students.OrderByDescending(s => s.GetMaxGrade()).First();
+        var studentWithAvgGrade = students.OrderByDescending(s => s.GetAverageGrade()).First();
+
+        Console.WriteLine($"Студент з максимальною оцінкою: {studentWithMaxGrade.Name} ({studentWithMaxGrade.GetMaxGrade()})");
+        Console.WriteLine($"Студент з середньою оцінкою: {studentWithAvgGrade.Name} ({studentWithAvgGrade.GetAverageGrade()})");
+    }
+}
---
+++ StudentGrade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+public class StudentGrade
+{
+    public string Name { get; set; }
+    public int[] Grades { get; set; }
+
+    public StudentGrade(string name, int[] grades)
+    {
+        Name = name;
+        Grades = grades;
+    }
+
+    public double GetMaxGrade()
+    {
+        return Grades.Max();
+    }
+
+    public double GetAverageGrade()
+    {
+        return Grades.Average();
+    }
+}
