3cda85880e5bb1b430f31f7fff55ec31a041edce
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:02:48 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Это строка для тестирования метода расширения WordCount.";
+        int wordCount = text.WordCount();
+        Console.WriteLine($"Количество слов в строке: {wordCount}");
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class StringExtensions
+{
+    public static int WordCount(this string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return 0;
+        }
+
+         string[] words = input.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        return words.Length;
+    }
+}
