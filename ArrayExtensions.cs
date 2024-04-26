5856815d63cdd9cfd71690db5db51c7d3f2259de
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:05:25 +0300

    1

---
+++ ArrayExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public static class ArrayExtensions
+{
+    public static int[] FilterArray(this int[] inputArray, Func<int, bool> predicate)
+    {
+        if (inputArray == null || predicate == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        return inputArray.Where(predicate).ToArray();
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        int[] evenNumbers = numbers.FilterArray(x => x % 2 == 0);
+
+        int[] oddNumbers = numbers.FilterArray(x => x % 2 != 0);
+
+        Console.WriteLine("Парні числа:");
+        foreach (var number in evenNumbers)
+        {
+            Console.Write(number + " ");
+        }
+
+        Console.WriteLine("\nНепарні числа:");
+        foreach (var number in oddNumbers)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
