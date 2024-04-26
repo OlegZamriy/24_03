9f7c636ea157d7e90b254dda84f533ba37033560
Олег Замрій <mynameishelg@gmail.com>
2024-04-26 19:18:08 +0300

    1

---
+++ PersonExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+public static class PersonExtensions
+{
+    public static event EventHandler<double> AverageAgeCalculated;
+
+    public static double CalculateAverageAge(this Person[] people)
+    {
+        double averageAge = people.Average(p => p.Age);
+        AverageAgeCalculated?.Invoke(null, averageAge);  
+        return averageAge;
+    }
+
+    public static Person FindPersonWithMinAge(this Person[] people)
+    {
+        return people.OrderBy(p => p.Age).FirstOrDefault();
+    }
+
+    public static Person FindPersonWithMaxAge(this Person[] people)
+    {
+        return people.OrderByDescending(p => p.Age).FirstOrDefault();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Person[] people = new Person[]
+        {
+            new Person("Іван", "Петров", 25),
+            new Person("Марія", "Іванова", 30),
+            new Person("Петро", "Сидоров", 20),
+            new Person("Анна", "Коваленко", 35)
+        };
+
+        Person personWithMinAge = people.FindPersonWithMinAge();
+        Person personWithMaxAge = people.FindPersonWithMaxAge();
+        double averageAge = people.CalculateAverageAge();
+
+        Console.WriteLine($"Особа з мінімальним віком: {personWithMinAge.FirstName} {personWithMinAge.LastName}, {personWithMinAge.Age} років");
+        Console.WriteLine($"Особа з максимальним віком: {personWithMaxAge.FirstName} {personWithMaxAge.LastName}, {personWithMaxAge.Age} років");
+        Console.WriteLine($"Середній вік осіб: {averageAge:F2} років");
+    }
+}
